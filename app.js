292b590991a11123cd4d306eaa0b986dd507404f
EXISTINGG <1906054192@qq.com>
2023-07-27 19:43:11 +0800

    rollback

--- app.js
+++ app.js
@@ -1,180 +1,109 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const imageType = require('image-type');
-const multer = require('multer');
-const decode = require('image-decode');
-const tf = require('@tensorflow/tfjs-node');
-const nsfw = require('nsfwjs');
+const express = require('express')
+const cors = require('cors') // cors中间件
+const multer = require('multer')
+const decode = require('image-decode')
 
-const app = express();
-const upload = multer();
-const MAX_IMAGES_ALLOWED = 10;
+const tf = require('@tensorflow/tfjs-node')
+const nsfw = require('nsfwjs')
 
-let _model;
+const app = express()
+const upload = multer()
 
-// 辅助函数：检查是否为图片类型
-async function isImageType(data) {
-  if (typeof data === 'string') {
-    try {
-      const response = await axios.head(data);
-      if (response.headers['content-type']) {
-        const contentType = response.headers['content-type'];
-        const type = imageType(Buffer.from(contentType, 'utf-8'));
-        return !!type;
-      }
-    } catch (error) {
-      return false;
-    }
-  } else if (data instanceof Buffer) {
-    const type = imageType(data);
-    return !!type;
-  }
-  return false;
-}
+let _model
 
 const convert = async (img) => {
-  const { data, width, height } = decode(img);
+  // image-decode模块用于解码图片
+  const { data, width, height } = decode(img)
 
-  const numChannels = 3;
-  const numPixels = width * height;
-  const values = new Int32Array(numPixels * numChannels);
+  const numChannels = 3
+  const numPixels = width * height
+  const values = new Int32Array(numPixels * numChannels)
 
   for (let i = 0; i < numPixels; i++)
     for (let c = 0; c < numChannels; ++c)
-      values[i * numChannels + c] = data[i * 4 + c];
+      values[i * numChannels + c] = data[i * 4 + c]
 
-  return tf.tensor3d(values, [height, width, numChannels], 'int32');
-};
+  return tf.tensor3d(values, [height, width, numChannels], 'int32')
+}
 
-app.use(cors());
+app.use(cors())
 
-// 单张图像 API
 app.post('/nsfw', upload.single('image'), async (req, res) => {
-  let imageData;
-  if (req.file) {
-    imageData = req.file.buffer;
-  } else if (req.body.imageUrl) {
-    const imageUrl = req.body.imageUrl;
-    const isValidImage = await isImageType(imageUrl);
-    if (!isValidImage) {
-      res.status(400).send('无效的图片链接或不是图片类型。');
-      return;
-    }
-
-    try {
-      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-      imageData = Buffer.from(response.data);
-    } catch (error) {
-      res.status(500).send('从图片链接获取图片数据时发生错误。');
-      return;
-    }
+  if (!req.file) {
+    res.status(400).send('Missing image multipart/form-data')
   } else {
-    res.status(400).send('请求中缺少图片数据或图片链接。');
-    return;
-  }
-
-  try {
-    const image = await convert(imageData);
-    const predictions = await _model.classify(image);
-    image.dispose();
+    const image = await convert(req.file.buffer)
+    const predictions = await _model.classify(image)
+    image.dispose()
 
     const formattedPredictions = predictions.map(
       ({ className, probability }) => ({
         className,
         probability: (probability * 100).toFixed(2) + '%'
       })
-    );
+    )
 
-    const sensitiveClasses = ['Hentai', 'Porn', 'Sexy'];
+    const sensitiveClasses = ['Hentai', 'Porn', 'Sexy']
     const isUnhealthy = formattedPredictions.some(
       ({ className, probability }) =>
         sensitiveClasses.includes(className) && parseFloat(probability) > 10
-    );
+    )
 
     const result = {
-      file: req.file ? req.file.originalname : req.body.imageUrl,
+      file: req.file.originalname,
       predictions: formattedPredictions,
       isHealthy: !isUnhealthy
-    };
+    }
 
-    res.json(result);
-  } catch (error) {
-    res.status(500).send('处理图片时发生错误。');
+    res.json(result)
   }
-});
+})
 
-// 多张图像 API
 app.post('/nsfws', upload.array('images', 10), async (req, res) => {
-  const imageUrls = req.body.imageUrls;
-  let imageDataArray = [];
-
-  if (req.files && req.files.length > 0) {
-    imageDataArray = req.files.map((file) => file.buffer);
-  } else if (Array.isArray(imageUrls) && imageUrls.length > 0) {
-    if (imageUrls.length > MAX_IMAGES_ALLOWED) {
-      res.status(400).send('最多支持10张图像。');
-      return;
-    }
-
-    const isValidImages = await Promise.all(imageUrls.map(isImageType));
-    if (!isValidImages.every((isValid) => isValid)) {
-      res.status(400).send('无效的图片链接或不是图片类型。');
-      return;
-    }
-
-    try {
-      const responses = await Promise.all(imageUrls.map((url) => axios.get(url, { responseType: 'arraybuffer' })));
-      imageDataArray = responses.map((response) => Buffer.from(response.data));
-    } catch (error) {
-      res.status(500).send('从图片链接获取图片数据时发生错误。');
-      return;
-    }
+  if (!req.files || req.files.length === 0) {
+    res.status(400).send('Missing image(s) multipart/form-data')
+  } else if (req.files.length > 10) {
+    res.status(400).send('最多支持10张')
   } else {
-    res.status(400).send('请求中缺少图片数据或图片链接。');
-    return;
-  }
-
-  try {
-    const promises = imageDataArray.map(async (imageData, index) => {
-      const image = await convert(imageData);
-      const predictions = await _model.classify(image);
-      image.dispose();
+    const promises = req.files.map(async (file) => {
+      const image = await convert(file.buffer)
+      const predictions = await _model.classify(image)
+      image.dispose()
 
       const formattedPredictions = predictions.map(
         ({ className, probability }) => ({
           className,
           probability: (probability * 100).toFixed(2) + '%'
         })
-      );
+      )
 
-      const sensitiveClasses = ['Hentai', 'Porn', 'Sexy'];
+      const sensitiveClasses = ['Hentai', 'Porn', 'Sexy']
       const isUnhealthy = formattedPredictions.some(
         ({ className, probability }) =>
           sensitiveClasses.includes(className) && parseFloat(probability) > 10
-      );
+      )
 
       const result = {
-        file: imageUrls[index],
+        file: file.originalname,
         predictions: formattedPredictions,
         isHealthy: !isUnhealthy
-      };
+      }
 
-      return result;
-    });
+      return result
+    })
 
-    const allResults = await Promise.all(promises);
-    res.json(allResults);
-  } catch (error) {
-    res.status(500).send('处理图片时发生错误。');
+    const allResults = await Promise.all(promises)
+    res.json(allResults)
   }
-});
+})
 
 const load_model = async () => {
-  _model = await nsfw.load();
-};
+  _model = await nsfw.load()
+}
 
-app.use((err, req, res, next) => res.status(500).send('发生错误：' + err.message));
+app.use((err, req, res, next) => res.send('发生错误,' + err.message))
 
-// 加载模型，并保持在内存中，确保只加载一次
-load_model().then(() => app.listen(80, () => console.log('服务器已启动')));
+// Keep the model in memory, make sure it's loaded only once
+load_model().then(() => app.listen(80, () => console.log('start')))
+
+// curl --request POST localhost:8080/nsfw --header 'Content-Type: multipart/form-data' --data-binary 'image=@/full/path/to/picture.jpg'
